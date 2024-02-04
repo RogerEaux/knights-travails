2a2f5ea522a2fdedb94dd08491554160314e01bc
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-04 17:12:35 -0600

    Add function to generate all possible valid moves

--- kightsTravails.js
+++ kightsTravails.js
@@ -1,3 +1,5 @@
+import createSquare from "./square";
+
 function translate(chessCords) {
   if (Number.isInteger(chessCords[0])) return chessCords;
 
@@ -8,4 +10,35 @@ function translate(chessCords) {
   ];
 
   return coords;
+}
+
+function makeMoves(square) {
+  //  A knight can only make 8 moves given by these coordinate adjustments
+  const adjustments = [
+    [-2, -1],
+    [-2, 1],
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
+  ];
+  const nextMoves = [];
+
+  //  Generate a square for each move the knight could move to
+  adjustments.forEach((adjustment) => {
+    const nextMove = createSquare(
+      square.x + adjustment[0],
+      square.y + adjustment[1],
+      square.path.push([square.x, square.y]),
+    );
+
+    //  If it exists on the board, add it
+    if (nextMove) {
+      nextMoves.push(nextMove);
+    }
+  });
+
+  return nextMoves;
 }
