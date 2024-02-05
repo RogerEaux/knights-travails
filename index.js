e18147a2df9048fc5989c3f03c7713719627227c
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-04 18:14:10 -0600

    Add driver test script

---
+++ index.js
@@ -0,0 +1,10 @@
+import knightTravails from './kightsTravails.js';
+
+//  Driver test script
+
+knightTravails([1, 2], [0, 0]);
+knightTravails(['a', 1], ['b', 3]);
+knightTravails([0, 0], [3, 3]);
+knightTravails(['d', 4], [0, 0]);
+knightTravails([0, 0], [7, 7]);
+knightTravails(['h', 8], ['a', 1]);
